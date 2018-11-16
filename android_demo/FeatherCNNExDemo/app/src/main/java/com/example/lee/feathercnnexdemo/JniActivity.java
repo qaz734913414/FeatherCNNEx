b77413814f4b35f737d349d1280e4c179e361ee6
lee <tianylijun@163.com>
2018-11-16 20:08:55 +0800

    Signed-off-by: lee <tianylijun@163.com>

--- android_demo/FeatherCNNExDemo/app/src/main/java/com/example/lee/feathercnnexdemo/JniActivity.java
+++ android_demo/FeatherCNNExDemo/app/src/main/java/com/example/lee/feathercnnexdemo/JniActivity.java
@@ -44,30 +44,36 @@ public class JniActivity extends AppCompatActivity implements View.OnClickListen
                 Log.e("Exception", e.getMessage(),e);
             }
         } else {
-            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.test);
-            //getEdge(bitmap);
-            EditText loopEdit = (EditText)findViewById(R.id.editLoop);
-            EditText threadsEdit = (EditText)findViewById(R.id.editThreads);
             EditText imgEdit = (EditText)findViewById(R.id.editImg);
-            EditText modelEdit = (EditText)findViewById(R.id.editModel);
-            EditText blobEdit = (EditText)findViewById(R.id.editBlob);
-            args.outLoopCnt = 1;
-            args.loopCnt = Integer.parseInt(loopEdit.getText().toString());
-            args.num_threads = Integer.parseInt(threadsEdit.getText().toString());;
-            args.bSameMean = 1;
-            args.bGray = 0;
-            args.b1x1Sgemm = 1;
-            args.bLowPrecision = 0;
-            args.viewType = 3;
-            args.pFname = "/sdcard/lj/"+imgEdit.getText().toString();
-            args.pModel = "/sdcard/lj/"+modelEdit.getText().toString();
-            args.pBlob = blobEdit.getText().toString();
-            args.pSerialFile = "/sdcard/lj/feather.key";
+            String url = "file:///sdcard/lj/"+imgEdit.getText().toString();
+            Log.d("lee-jni-test", url);
+            ContentResolver cr = this.getContentResolver();
+            try {
+                Bitmap bitmap = BitmapFactory.decodeStream(cr.openInputStream(Uri.parse(url)));
+                EditText loopEdit = (EditText)findViewById(R.id.editLoop);
+                EditText threadsEdit = (EditText)findViewById(R.id.editThreads);
+                EditText modelEdit = (EditText)findViewById(R.id.editModel);
+                EditText blobEdit = (EditText)findViewById(R.id.editBlob);
+                args.outLoopCnt = 1;
+                args.loopCnt = Integer.parseInt(loopEdit.getText().toString());
+                args.num_threads = Integer.parseInt(threadsEdit.getText().toString());;
+                args.bSameMean = 1;
+                args.bGray = 0;
+                args.b1x1Sgemm = 1;
+                args.bLowPrecision = 0;
+                args.viewType = 3;
+                args.pFname = "/sdcard/lj/"+imgEdit.getText().toString();
+                args.pModel = "/sdcard/lj/"+modelEdit.getText().toString();
+                args.pBlob = blobEdit.getText().toString();
+                args.pSerialFile = "/sdcard/lj/feather.key";
 
-            int avgTime = FeatherCNNExTest(args, bitmap);
-            TextView avgTimeText = (TextView)findViewById(R.id.textViewAvgTime);
-            avgTimeText.setText("AvgTime: "+avgTime+" ms");
-            imageView.setImageBitmap(bitmap);
+                int avgTime = FeatherCNNExTest(args, bitmap);
+                TextView avgTimeText = (TextView)findViewById(R.id.textViewAvgTime);
+                avgTimeText.setText("AvgTime: "+avgTime+" ms");
+                imageView.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                Log.e("Exception", e.getMessage(),e);
+            }
         }
     }
 
